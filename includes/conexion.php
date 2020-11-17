21346fea5ec501e06be2e7d45aafbab761f5aa02
jucetina <juliethcetina@gmail.com>
2020-11-16 20:18:35 -0500

    Se agrega comentario a un helper redireccion.php

--- includes/conexion.php
+++ includes/conexion.php
@@ -2,6 +2,8 @@
 //Conexión con la base de datos blog
 
 $db = mysqli_connect('localhost', 'root', 'root', 'blog');
+//Conexión al proyecto montado en Heroku:
+//$db = mysqli_connect('us-cdbr-east-02.cleardb.com', 'b24f49366375c8', '2960ec11', 'heroku_aa40eae22e4a3b6');
 mysqli_query($db, "SET NAMES 'utf8'");
 
 //Iniciar sesión
--- includes/redireccion.php
+++ includes/redireccion.php
@@ -1,5 +1,6 @@
 <?php
 
+//Redirección para que envíe al indexphp si el usuario no está logueado
 if(!isset($_SESSION['usuario'])){
     header('Location:index.php');
 }
