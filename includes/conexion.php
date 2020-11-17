eb0d57b98418648b941a21c0217f447520e48eca
jucetina <juliethcetina@gmail.com>
2020-11-16 20:10:14 -0500

    Se corrige el tema de archivos de conexion a bd local

---
+++ includes/conexion.php
@@ -0,0 +1,8 @@
+<?php
+//Conexión con la base de datos blog
+
+$db = mysqli_connect('localhost', 'root', 'root', 'blog');
+mysqli_query($db, "SET NAMES 'utf8'");
+
+//Iniciar sesión
+session_start();
