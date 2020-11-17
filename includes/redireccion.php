aa9efdc8b2c91c6a0a21f3e64d34fe1d4b7dbf8b
jucetina <juliethcetina@gmail.com>
2020-11-16 20:20:14 -0500

    Se agrega comentario a un helper redireccion.php mejorado

--- includes/redireccion.php
+++ includes/redireccion.php
@@ -1,6 +1,6 @@
 <?php
 
-//Redirección para que envíe al indexphp si el usuario no está logueado
+//Redirección para que envíe al index.php (a la pantalla de inicio) si el usuario no está logueado
 if(!isset($_SESSION['usuario'])){
     header('Location:index.php');
 }
