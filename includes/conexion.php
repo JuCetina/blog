2d9fe9fece0f808e0cd598cfab6ef4558c9c8b63
jucetina <juliethcetina@gmail.com>
2020-11-16 20:06:47 -0500

    Se eliminan los archivos de conexion a la bd

--- includes/conexion.php
+++
@@ -1,9 +0,0 @@
-<?php
-//Conexión con la base de datos blog
-
-//$db = mysqli_connect('localhost', 'root', 'root', 'blog');
-$db = mysqli_connect('us-cdbr-east-02.cleardb.com', 'b24f49366375c8', '2960ec11', 'heroku_aa40eae22e4a3b6');
-mysqli_query($db, "SET NAMES 'utf8'");
-
-//Iniciar sesión
-session_start();
